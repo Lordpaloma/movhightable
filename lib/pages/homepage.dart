2d3aa8d4ace99dae7f6c03b04dd6853f5382c77d
JOlave <jaime.olave@usm.cl>
2024-04-21 01:49:37 -0400

    textthemee

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,9 +6,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Inicio'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Inicio'),
+        ),
+        body: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text('Bienvenido a la aplicación del Continental',style: Theme.of(context).textTheme.bodyText1, )
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 27, 27, 31));
   }
 }
