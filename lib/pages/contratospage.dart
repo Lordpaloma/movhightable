0b4723de2d2c682c43b67705cac61f3617efa76b
JOlave <jaime.olave@usm.cl>
2024-04-22 15:02:36 -0400

    Contratos y algunas imagenes que olvidare borrar

--- lib/pages/contratospage.dart
+++ lib/pages/contratospage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movhightable/Widget/contratos.dart';
 
 class ContratosPage extends StatelessWidget {
   const ContratosPage({super.key});
@@ -6,8 +7,46 @@ class ContratosPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      backgroundColor: Color.fromARGB(255, 27, 27, 31)
+      appBar: AppBar(
+        title: Text('Contratos'),
+      ),
+      backgroundColor: Color.fromARGB(255, 27, 27, 31),
+      body: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Contratos(
+                  nombre: 'Jhon Wick',
+                  foto: 'ProPicJhonwic.jpg',
+                  Info: 'Lorem ipsum dolor sit amet.',
+                  monto: '10000'),
+              Contratos(
+                  nombre: 'Lorem ipsum',
+                  foto: 'inicio.png',
+                  Info: 'Lorem ipsum dolor sit amet',
+                  monto: '100'),
+              Contratos(
+                  nombre: 'Lorem ipsum',
+                  foto: 'inicio.png',
+                  Info: 'Lorem ipsum dolor sit amet',
+                  monto: '100'),
+              Contratos(
+                  nombre: 'Lorem ipsum',
+                  foto: 'inicio.png',
+                  Info: 'Lorem ipsum dolor sit amet',
+                  monto: '100'),
+              Contratos(
+                  nombre: 'Lorem ipsum',
+                  foto: 'inicio.png',
+                  Info: 'Lorem ipsum dolor sit amet',
+                  monto: '100')
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
